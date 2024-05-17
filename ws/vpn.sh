30ca64b33173e263201c1c8f99b8dfad1861644a
LunaticBackend <157736966+LunaticBackend@users.noreply.github.com>
2024-05-17 12:40:46 +0700

    Add files via upload

--- ws/vpn.sh
+++ ws/vpn.sh
@@ -25,7 +25,7 @@ fi
 # Mod By SL
 # ==================================================
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/senowahyu62/scriptvps/main/ssh"
+akbarvpn="raw.githubusercontent.com/LunaticBackend/vpnscript/mytich/ws"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
